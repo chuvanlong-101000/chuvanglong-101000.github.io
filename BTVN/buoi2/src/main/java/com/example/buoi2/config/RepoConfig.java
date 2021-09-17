76f302b9d9cbb3b009674370e086f7932d6ad944
chuvanlong <chuvanlong.qle@gmail.com>
2021-09-17 10:50:51 +0700

    update

---
+++ BTVN/buoi2/src/main/java/com/example/buoi2/config/RepoConfig.java
@@ -0,0 +1,12 @@
+package com.example.buoi2.config;
+import com.example.buoi2.repository.BookDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class RepoConfig {
+    
+    @Bean
+    public BookDao bookDao() {
+        return new BookDao();
+    }
+}
